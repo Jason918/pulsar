b4725fcd77cee02d211d9dc903864cf654dfe366
suiyuzeng <suiyuzeng@didiglobal.com>
2021-11-16 11:12:18 +0800

    perf config

--- build_bk/899-logsymbol.required.sh
+++ build_bk/899-logsymbol.required.sh
@@ -19,6 +19,7 @@
 #
 
 mkdir -p /home/xiaoju/data1/pulsar_bk/logs
+rm -rf /home/xiaoju/pulsar_bk/logs
 ln -s /home/xiaoju/data1/pulsar_bk/logs /home/xiaoju/pulsar_bk/logs
 
 chown -R xiaoju.xiaoju /home/xiaoju/pulsar_bk
